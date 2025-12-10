d42da8f96d669f3247601c83f1d01761fc70f555
QM719 <80386214+QM719@users.noreply.github.com>
2025-12-10 11:07:00 +0800

    Lock security level to 'low' only
    The security level is now locked to 'low' only, removing options for other levels.

--- security.php
+++ security.php
@@ -14,21 +14,8 @@ if( isset( $_POST['seclev_submit'] ) ) {
 	// Anti-CSRF
 	checkToken( $_REQUEST[ 'user_token' ], $_SESSION[ 'session_token' ], 'security.php' );
 
-	$securityLevel = '';
-	switch( $_POST[ 'security' ] ) {
-		case 'low':
-			$securityLevel = 'low';
-			break;
-		case 'medium':
-			$securityLevel = 'medium';
-			break;
-		case 'high':
-			$securityLevel = 'high';
-			break;
-		default:
-			$securityLevel = 'impossible';
-			break;
-	}
+	// Force security level to 'low' only
+	$securityLevel = 'low';
 
 	dvwaSecurityLevelSet( $securityLevel );
 	dvwaMessagePush( "Security level set to {$securityLevel}" );
@@ -38,14 +25,11 @@ if( isset( $_POST['seclev_submit'] ) ) {
 
 $securityOptionsHtml = '';
 $securityLevelHtml   = '';
-foreach( array( 'low', 'medium', 'high', 'impossible' ) as $securityLevel ) {
-	$selected = '';
-	if( $securityLevel == dvwaSecurityLevelGet() ) {
-		$selected = ' selected="selected"';
-		$securityLevelHtml = "<p>Security level is currently: <em>$securityLevel</em>.<p>";
-	}
-	$securityOptionsHtml .= "<option value=\"{$securityLevel}\"{$selected}>" . ucfirst($securityLevel) . "</option>";
-}
+// Only allow 'low' security level
+$securityLevel = 'low';
+$selected = ' selected="selected"';
+$securityLevelHtml = "<p>Security level is currently: <em>$securityLevel</em> (locked to low only).<p>";
+$securityOptionsHtml .= "<option value=\"{$securityLevel}\"{$selected}>" . ucfirst($securityLevel) . "</option>";
 
 // Anti-CSRF
 generateSessionToken();
@@ -61,19 +45,16 @@ $page[ 'body' ] .= "
 
 	<form action=\"#\" method=\"POST\">
 		{$securityLevelHtml}
-		<p>You can set the security level to low, medium, high or impossible. The security level changes the vulnerability level of DVWA:</p>
+		<p><strong>Security level is locked to LOW only.</strong> The security level changes the vulnerability level of DVWA:</p>
 		<ol>
 			<li> Low - This security level is completely vulnerable and <em>has no security measures at all</em>. It's use is to be as an example of how web application vulnerabilities manifest through bad coding practices and to serve as a platform to teach or learn basic exploitation techniques.</li>
-			<li> Medium - This setting is mainly to give an example to the user of <em>bad security practices</em>, where the developer has tried but failed to secure an application. It also acts as a challenge to users to refine their exploitation techniques.</li>
-			<li> High - This option is an extension to the medium difficulty, with a mixture of <em>harder or alternative bad practices</em> to attempt to secure the code. The vulnerability may not allow the same extent of the exploitation, similar in various Capture The Flags (CTFs) competitions.</li>
-			<li> Impossible - This level should be <em>secure against all vulnerabilities</em>. It is used to compare the vulnerable source code to the secure source code.<br />
-				Prior to DVWA v1.9, this level was known as 'high'.</li>
 		</ol>
 		<select name=\"security\">
 			{$securityOptionsHtml}
 		</select>
 		<input type=\"submit\" value=\"Submit\" name=\"seclev_submit\">
 		" . tokenField() . "
+		<p><em>Note: Security level is locked to LOW. Other levels are not available.</em></p>
 	</form>
 	
 	<br>
